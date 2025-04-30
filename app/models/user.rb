0bb2e635adbf25e7c620ad0b2580cff04d8b4cf7
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-30 13:54:38 -0400

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  VALID_ROLES = %w[student staff]
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true
+  validates :role, inclusion: { in: VALID_ROLES }
 end
