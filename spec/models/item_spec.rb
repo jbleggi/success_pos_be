62640560f2150b33905392d2f8ff954f263fd23f
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-30 14:02:39 -0400

    Create item validation spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
+# spec/models/item_spec.rb
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:item) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price) }
+  it { should validate_numericality_of(:quantity).only_integer.allow_nil }
+
+  it "has a valid factory" do
+    expect(subject).to be_valid
+  end
 end
