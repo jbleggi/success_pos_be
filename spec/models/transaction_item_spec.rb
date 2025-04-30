c04a8789f855147dae5aea5962309d5b463d4302
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-30 14:09:37 -0400

    Created Transaction Item validation model spec

--- spec/models/transaction_item_spec.rb
+++ spec/models/transaction_item_spec.rb
@@ -1,5 +1,18 @@
+# spec/models/transaction_item_spec.rb
 require 'rails_helper'
 
 RSpec.describe TransactionItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:transaction_item) }
+
+  it { should belong_to(:order).class_name("Transaction") }
+  it { should belong_to(:item) }
+
+  it { should validate_presence_of(:quantity) }
+  it { should validate_numericality_of(:quantity).only_integer }
+  it { should validate_presence_of(:price_at_sale) }
+  it { should validate_numericality_of(:price_at_sale) }
+
+  it "has a valid factory" do
+    expect(subject).to be_valid
+  end
 end
