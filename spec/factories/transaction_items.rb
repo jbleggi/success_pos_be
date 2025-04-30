a505fbc96d25776789759e4cefe0567807ce1130
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-30 14:09:23 -0400

    Edited order with class name transaction

--- spec/factories/transaction_items.rb
+++ spec/factories/transaction_items.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :transaction_item do
-    association :transaction
+    association :order, factory: :transaction
     association :item
     quantity { rand(1..3) }
     price_at_sale { item.price }
