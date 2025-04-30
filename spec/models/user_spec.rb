a3e39c73e85cccc3c2f724296b82134d4afef8e7
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-30 13:56:20 -0400

    Create user validation model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
+# spec/models/user_spec.rb
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+
+  it "has a valid factory" do
+    expect(subject).to be_valid
+  end
 end
