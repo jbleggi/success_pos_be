928882f09905ab6ec01a57d23c1360c7c715d9f0
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-04-30 14:07:48 -0400

    Fixed single quote to double

--- app/models/transaction_item.rb
+++ app/models/transaction_item.rb
@@ -1,5 +1,5 @@
 class TransactionItem < ApplicationRecord
-belongs_to :order, class_name: Transaction, foreign_key: transaction_id
+belongs_to :order, class_name: "Transaction", foreign_key: "transaction_id"
   belongs_to :item
 
   validates :quantity, presence: true, numericality: { only_integer: true }
